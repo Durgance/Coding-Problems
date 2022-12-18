63f2743c60acc7871308a10c36a1838d3e67e4d0
Durgance Gaur <gdurgance@gmail.com>
2022-12-19 03:01:00 +0530

    Time: 2 ms (91.39%), Space: 46 MB (86.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int K = nums.length/2;
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue()>K) return entry.getKey();
-}return 1;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
