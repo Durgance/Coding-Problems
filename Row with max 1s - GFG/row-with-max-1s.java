52a07d9788fbdcf782c4afd90905df4d1e985fb7
Durgance Gaur <gdurgance@gmail.com>
2022-12-19 02:46:16 +0530

    Added solution - LeetHub

---
+++ Row with max 1s - GFG/row-with-max-1s.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+        
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+        // code here
+        // Queue<Integer> pq_max = new PriorityQueue<>(Comparator.reverseOrder());
+        int i=0,sum=0;
+        int currSum=0,idx=0;
+        for(int[] a: arr){
+            currSum =Arrays.stream(a).sum();
+            if (currSum>sum){
+                sum = currSum;    
+                idx = i;
+            } 
+            i++;
+                        
+        }
+        if (sum==0) return -1;
+        return idx;
+        
+    }
+}
