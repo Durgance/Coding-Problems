5e09a4e15a85c58a1942aa30a10808cfa93a0711
Durgance Gaur <gdurgance@gmail.com>
2022-12-19 15:34:30 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.java
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -43,6 +43,25 @@ class Solution{
         }
         return maxSum;
         
+        // Kadane's Algorithm
+        /*
+        int msf = Integer.MIN_VALUE;
+        int meh = 0,s=0;
+        int start,end;
+        for(int i=0;i<arr.length;i++){
+            meh += arr[i];
+            if(meh>msf){
+                msf = meh;
+                start =s;
+                end = i;
+            } 
+            if (meh<0){
+                meh = 0;
+                s = i+1;
+            }
+        }
+        return msf;
+        */
     }
     
 }
