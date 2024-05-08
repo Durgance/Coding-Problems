233f052e69451ec4874b6e8b2b1aaa83aea42000
Durgance Gaur <gdurgance@gmail.com>
2024-05-08 23:58:59 +0530

    Time: 45 ms (49.57%), Space: 16.5 MB (62.90%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # sys.set_int_max_str_digits(1000000000)
+        # num1 = int(num1)
+        # num2 = int(num2)
+        return str(int(num1) * int(num2))
