4792101688e61213a2c583c91e6c2860dedaf617
Durgance Gaur <gdurgance@gmail.com>
2025-11-27 01:47:08 +0530

    Time: 359 ms (98.33%) | Memory: 20.7 MB (71.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,21 +1,37 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        triplets = set()
-
-        nums.sort()
+    def threeSum(self, arr: List[int]) -> List[List[int]]:
+        triplets = []
+        # TODO: Write your code here
+        arr.sort()
+        # print(arr)
         i = 0
-        while i<len(nums):
-            target = -1*nums[i]
+        # left = 0
+        right = len(arr) - 1
+        for i in range(len(arr)):
+            if i>0 and arr[i]==arr[i-1]:
+                continue
+            if arr[i]>0:
+                break
+
+            target = -1*arr[i]
             left = i+1
-            right = len(nums)-1
+            right = len(arr) - 1
             while left<right:
-                curr_sum = nums[left] + nums[right]
-                if curr_sum<target:
-                    left+=1
-                elif curr_sum>target:
-                    right-=1
+                curr_sum = arr[left] +arr[right]
+                if curr_sum == target:
+                    triplets.append([arr[i],arr[left],arr[right]])
+                    # print(triplets)
+                    left += 1
+                    right -= 1
+                    while left<right and arr[left]==arr[left-1]:
+                        left += 1
+                    while left<right and arr[right]==arr[right+1]:
+                        right -= 1
+                elif curr_sum > target:
+                    right -= 1
                 else:
-                    triplets.add((nums[i],nums[left],nums[right]))
-                    left+=1
-            i+=1
-        return list(map(list,triplets))
+                    left += 1
+                # right = len(arr) - 1
+
+            # i += 1
+        return triplets
