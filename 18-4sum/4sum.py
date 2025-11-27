8e8b515a9f9c39d9285340f3589a6332780644a9
Durgance Gaur <gdurgance@gmail.com>
2025-11-27 23:57:41 +0530

    Time: 554 ms (16.57%) | Memory: 17.9 MB (36.53%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fourSum(self, arr: List[int], target: int) -> List[List[int]]:
+        # quadruplets = []
+        quadruplets = set()
+        arr.sort()
+        for i in range(len(arr)-3):
+            for j in range(i+1,len(arr)-2):
+                # value = target-arr[i] -arr[j]     
+                if i!=j :
+                    left = j + 1 
+                    right = len(arr)-1
+                    while left<right:
+                        total = arr[left]+arr[right] +arr[i] + arr[j]
+                        if total == target and i!=j and left!=right and i!=left and i!=right and j!=left and j!=right:
+                            quadruplets.add(tuple(sorted([arr[i],arr[j],arr[left],arr[right]])))
+                            # quadruplets.append([arr[i],arr[j],arr[left],arr[right]])
+                            left += 1
+                            right -= 1
+                            while left<right and arr[left]== arr[left-1]:
+                                left += 1
+                            while left<right and arr[right] == arr[right+1]:
+                                right -= 1
+                        elif total<target:
+                            left += 1
+                        else:
+                            right -= 1
+        # quadruplets = sorted(quadruplets)
+        # print(list(map(list,quadruplets))
+        # return list(map(list,quadruplets))
+        # return quadruplets
+        return list(map(list,quadruplets))
