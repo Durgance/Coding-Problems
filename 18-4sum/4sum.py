d6ededfe7f4cc9a09b974593febf46d870978150
Durgance Gaur <gdurgance@gmail.com>
2025-11-28 00:00:40 +0530

    Time: 431 ms (24.55%) | Memory: 17.7 MB (82.35%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def fourSum(self, arr: List[int], target: int) -> List[List[int]]:
-        # quadruplets = []
-        quadruplets = set()
+        quadruplets = []
+        # quadruplets = set()
         arr.sort()
         for i in range(len(arr)-3):
+            if i>0 and arr[i] == arr[i-1]: continue # skip i dedup
             for j in range(i+1,len(arr)-2):
+                if j>i+1 and arr[j]==arr[j-1]: continue # skip j dedup 
                 # value = target-arr[i] -arr[j]     
                 if i!=j :
                     left = j + 1 
@@ -12,8 +14,8 @@ class Solution:
                     while left<right:
                         total = arr[left]+arr[right] +arr[i] + arr[j]
                         if total == target and i!=j and left!=right and i!=left and i!=right and j!=left and j!=right:
-                            quadruplets.add(tuple(sorted([arr[i],arr[j],arr[left],arr[right]])))
-                            # quadruplets.append([arr[i],arr[j],arr[left],arr[right]])
+                            # quadruplets.add(tuple(sorted([arr[i],arr[j],arr[left],arr[right]])))
+                            quadruplets.append([arr[i],arr[j],arr[left],arr[right]])
                             left += 1
                             right -= 1
                             while left<right and arr[left]== arr[left-1]:
@@ -27,5 +29,5 @@ class Solution:
         # quadruplets = sorted(quadruplets)
         # print(list(map(list,quadruplets))
         # return list(map(list,quadruplets))
-        # return quadruplets
-        return list(map(list,quadruplets))
+        return quadruplets
+        # return list(map(list,quadruplets))
