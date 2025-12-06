d5eeb4c5b5c5638031c4a897cef460d409c07462
Durgance Gaur <gdurgance@gmail.com>
2025-12-06 16:56:27 +0530

    Time: 3 ms (41.69%) | Memory: 17.7 MB (60.54%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -8,13 +8,19 @@ class Solution:
         return s
     
     def isHappy(self, n: int) -> bool:
-        count = 0
+        # count = 0
+        slow ,fast = n, n
         while True:
-            val = self.get_sum(n)
-            if val ==1:
+            slow = self.get_sum(slow)
+            fast = self.get_sum(fast)
+            fast = self.get_sum(fast)
+
+            if slow ==1 or fast == 1:
                 return True
-            n = val
-            if count>=10:
+            elif slow == fast:
                 return False
-            count +=1
-        return False
+            # n = val
+            # if count>=10:
+            #     return False
+            # count +=1
+        # return False
