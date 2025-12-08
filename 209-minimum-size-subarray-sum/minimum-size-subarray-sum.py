45ba2da0c4e97adeb8676623e593f1299dd80e77
Durgance Gaur <gdurgance@gmail.com>
2025-12-09 02:40:24 +0530

    Time: 10 ms (52.16%) | Memory: 28.2 MB (80.73%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -3,7 +3,7 @@ class Solution:
         start ,end , win_sum = 0, 0, 0
         min_len = float('inf')
         # flag = False
-        while end<len(nums):
+        for end in range(len(nums)):
             win_sum += nums[end]
             while win_sum>=target:
                 # flag = True
@@ -12,8 +12,8 @@ class Solution:
                 start+=1
                 # end = start
                 # win_sum = 0
-            else:
-                end+=1
+            # else:
+            #     end+=1
         # if flag == False:
         #     return 0
         # else:
