17bc3cd926667e8e9ccec18b10575d76e351b810
Durgance Gaur <gdurgance@gmail.com>
2025-12-12 02:20:00 +0530

    Time: 31 ms (83.84%) | Memory: 18.6 MB (16.11%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,15 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s):
+        n_s = len(s)
+        n_p = len(p)
+        if n_p>n_s:
             return []
+
         start = 0
         ans = []
         matched = 0
         freq_map = {}
         for char in p:
             freq_map[char] = 1 + freq_map.get(char,0)
-        n = len(s)
-        for end in range(n):
+        for end in range(n_s):
             right_char = s[end]
             if right_char in freq_map:
                 freq_map[right_char] -= 1
@@ -19,7 +21,7 @@ class Solution:
             if matched == len(freq_map):
                 ans.append(start)
             
-            if end - start +1 >= len(p):
+            if end - start +1 >= n_p:
                 left_char = s[start]
                 start+=1
                 if left_char in freq_map:
