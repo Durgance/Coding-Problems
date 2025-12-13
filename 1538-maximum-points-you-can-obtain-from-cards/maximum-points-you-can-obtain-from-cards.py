c08936c97ef9350c6775e0abb6f59fdbae624c47
Durgance Gaur <gdurgance@gmail.com>
2025-12-13 20:13:29 +0530

    Time: 39 ms (22.22%) | Memory: 28.2 MB (19.53%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,11 +1,15 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
+        
+        # https://chatgpt.com/share/693b3b60-e7a0-800f-ace7-ac926e4f4d04 understanding Window function
+        # this problem intution https://chatgpt.com/share/693b3b60-e7a0-800f-ace7-ac926e4f4d04
+        
         start = 0
         win_sum = 0
         # min_sum = 0
         min_sum = float('inf')
         n = len(cardPoints)
-
+        
         freq_map = {}
         for end in range(n):
             right_card = cardPoints[end]
